47e1bb05c5a9b6455c411f952f527a7253c35b76
Michael J. Roberts <michael@observerly.com>
2022-05-26 14:22:30 +0100

    ci: Added jsdom to the vitest test runner.
    ci: Added jsdom to the vitest test runner. Includes updating vite.config.ts for test setup and reference types for vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'jsdom',
+  }
 })
