1921ac80071ca371e9e4fde1ff42c11c6e21b76a
Michael J. Roberts <michael@observerly.com>
2022-05-30 13:05:55 +0100

    feat: Improved type inference for useTRPCQuery().

--- src/composables/useTRPCQuery.ts
+++ src/composables/useTRPCQuery.ts
@@ -4,30 +4,13 @@ import type {
   ProcedureRecord,
   inferHandlerInput,
   inferProcedureInput,
-  inferProcedureOutput
+  inferProcedureOutput,
+  ThenArg
 } from '@trpc/server'
 
 import { AnyRouter } from '@trpc/server'
 
-import type { TRPCClient, TRPCClientErrorLike } from '@trpc/client'
-
-/**
- *
- * This is a helper method to infer the output of a query resolver
- *
- */
-export type inferQueryOutput<TRouter extends AnyRouter, TRouteKey extends keyof TRouter['_def']['queries']> = inferProcedureOutput<
-  TRouter['_def']['queries'][TRouteKey]
->
-
-/**
- *
- * This is a helper method to infer the input of a query resolver
- *
- */
-export type inferQueryInput<TRouter extends AnyRouter, TRouteKey extends keyof TRouter['_def']['queries']> = inferProcedureInput<
-  TRouter['_def']['queries'][TRouteKey]
->
+import type { TRPCClient } from '@trpc/client'
 
 export type inferProcedures<TObj extends ProcedureRecord<any, any, any, any, any, any>> = {
   [TPath in keyof TObj]: {
@@ -36,23 +19,19 @@ export type inferProcedures<TObj extends ProcedureRecord<any, any, any, any, any
   }
 }
 
-export type TQueries<TRouter extends AnyRouter> = TRouter['_def']['queries']
-
-export type TError<TRouter extends AnyRouter> = TRPCClientErrorLike<TRouter>
-
-export type TQueryValues<TRouter extends AnyRouter> = inferProcedures<TRouter['_def']['queries']>
-
-export type TPath<TRouter extends AnyRouter> = keyof TQueryValues<TRouter> & string
-
-export function useTRPCQuery<TRouter extends AnyRouter>(
-  client: TRPCClient<TRouter>,
-  path: TPath<TRouter>, 
-  ...args: inferHandlerInput<TQueries<TRouter>[TPath<TRouter>]>
+export function useTRPCQuery<
+  AppRouter extends AnyRouter,
+  TPath extends keyof inferProcedures<AppRouter['_def']['queries']> & string
+>(
+  client: TRPCClient<AppRouter>,
+  pathAndInput: [path: TPath, ...args: inferHandlerInput<AppRouter['_def']['queries'][TPath]>]
 ) {
-  const response = ref<inferQueryOutput<TRouter, TPath<TRouter>>>()
+  const response = ref<Awaited<ThenArg<ReturnType<AppRouter['_def']['queries'][TPath]['call']>>>>()
 
   ;(async () => {
-    const data = await client.query(path, ...args)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const data = await client.query(...pathAndInput)
     response.value = data
   })()
 
